019079f6509189857e481409c0a494b594863a4c
Aktous <rtalhamrite@gmail.com>
2023-06-19 19:00:51 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+      *main - Positive anything is better than negative nothing
+      *Return: always (success)
+      *
+      */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
