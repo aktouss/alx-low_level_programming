d9a4fa359851ac03ae63f23749c069f0a1f22068
Aktous <rtalhamrite@gmail.com>
2023-06-22 10:41:54 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper -  checks for a digit
+ * @s: The number to be checked
+ * Return: Always 0.
+ */
+
+int _isupper(int s)
+{
+	if (s >= 65 && s <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
