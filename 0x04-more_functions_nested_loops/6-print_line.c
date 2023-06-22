d2e5e8271c2ff67defb5e3170f95dfe05f18ef5f
Aktous <rtalhamrite@gmail.com>
2023-06-22 14:16:08 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *@n: The numbre of lines to draw
+ * Return: empty
+ */
+
+void print_numbers(int n)
+{
+	int x;
+
+	if (n >= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < n; x++)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+	}
+}
+
+
